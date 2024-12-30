42af0fd5df8616559980e1c89bcd3f330a74cbb7
Artem <artembodrik210311@gmail.com>
2024-12-30 15:12:35 +0200

    push_lol

---
+++ javascript.js
@@ -0,0 +1,24 @@
+let age = 13;
+var name = "Artem";
+let isStudent = true;
+let myString = "true или false вот в чем вопрос";
+let myNumber = 5 + 10;
+// console.log(myNumber+10)
+let myNull = "hull";
+console.log(myNull)
+var name = prompt("ваше имя?")
+let di = confirm("подтвердите действие!")
+if (di == true) {
+console.log("Спасибо за подтверждение!")
+}
+else {
+console.log("Действие отменено!")
+}
+alert("Действие опасное подтвердите действие")
+let ifo = confirm("подтвердите действие!")
+if (ifo == true) {
+    console.log("Спасибо за подтверждение!")
+    }
+    else {
+    console.log("Действие отменено!")
+    }

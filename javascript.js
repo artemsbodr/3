f36d968e811ab7220069659b41809b3dd11d764f
Artem <artembodrik210311@gmail.com>
2025-01-06 18:11:13 +0200

    push2

--- javascript.js
+++ javascript.js
@@ -3,7 +3,10 @@ var name = "Artem";
 let isStudent = true;
 let myString = "true или false вот в чем вопрос";
 let myNumber = 5 + 10;
-// console.log(myNumber+10)
+console.log(myNumber)
+console.log(age, "возраст")
+console.log(name, "имя")
+console.log(myString,"цитата")
 let myNull = "hull";
 console.log(myNull)
 var name = prompt("ваше имя?")

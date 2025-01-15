f3ab24b09a1eddba579ed99a8487196f4977ad05
Artem <artembodrik210311@gmail.com>
2025-01-15 18:49:26 +0200

    lol

---
+++ L3@1/javase.js
@@ -0,0 +1,14 @@
+const result = 5+5+5;
+const payment = "400 999 гривень!";
+const userName = "Viktor";
+let m = "My ";
+let n = "name ";
+let i = "is ";
+const email = "posta@.com";
+let fullName = m + n + i + "Viktor";
+console.log(fullName);
+console.log(result);
+alert("Дякуємо, "+ userName + " До сплати " + payment)
+if (email.includes("@")) {
+    console.log(email)
+}

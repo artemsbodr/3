a3a8ea5458235a3960e7afe31e3b3f10a2fc327e
Artem <artembodrik210311@gmail.com>
2025-02-10 18:12:26 +0200

    push_LOL

--- javase.js
+++ javase.js
@@ -1,15 +1,19 @@
+// завдання 1
 console.log("Hi")
 const result = 5+5+5;
-const payment = "400 999 гривень!";
-const userName = "Viktor";
-let m = "My ";
-let n = "name ";
-let i = "is ";
+// завдання 
 const email = "posta@.com";
-let fullName = m + n + i + "Viktor";
-console.log(fullName);
-console.log(result);
-alert("Дякуємо, "+ userName + " До сплати " + payment)
 if (email.includes("@")) {
     console.log(email)
 }
+// завдання 3
+let m = "My ";
+let n = "name ";
+let i = "is ";
+let fullName = m + n + i + "Viktor";
+console.log(fullName);
+console.log(result);
+// завдання 4
+const payment = "400 999 гривень!";
+const userName = "Viktor";
+alert("Дякуємо, "+ userName + " До сплати " + payment);

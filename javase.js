7ee98e3b6563491523558e1ccada54cbc2fd221d
Artem <artembodrik210311@gmail.com>
2025-02-17 18:10:55 +0200

    push

--- javase.js
+++ javase.js
@@ -1,19 +1,20 @@
 // завдання 1
-console.log("Hi")
-const result = 5+5+5;
-// завдання 2
-const email = "posta@.com";
-if (email.includes("@")) {
-    console.log(email)
-}
-// завдання 3
-let m = "My ";
-let n = "name ";
-let i = "is ";
-let fullName = m + n + i + "Viktor";
-console.log(fullName);
+let result = 5 + 5 + '5';
 console.log(result);
+console.log(typeof result);
+// завдання 2
+let email = 'your.email@example.com';
+let containsAt = email.includes('@');
+let length = email.length;
+console.log(`Email містить символ @: ${containsAt}`);
+console.log(`Загальна кількість символів: ${length}`);
+// завдання 3
+let word1 = 'My';
+let word2 = 'name';
+let word3 = 'is';
+let fullName = `${word1} ${word2} ${word3} Viktor`;
+console.log(fullName);
 // завдання 4
-const payment = "400 999 гривень!";
-const userName = "Viktor";
-alert("Дякуємо, "+ userName + " До сплати " + payment);
+let userName = 'Олександро';
+let payment = 3600;
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
